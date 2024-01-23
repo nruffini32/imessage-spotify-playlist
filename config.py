14b79ac1eff63ac0590974bf3e87ea135b59b2d7
nruffini32 <71286321+nruffini32@users.noreply.github.com>
2024-01-22 18:35:01 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-GROUP_CHAT = "Chunes"
-DB_PATH = "/Users/nicoloruffini/Library/Messages/chat.db"
+GROUP_CHAT = ""
+DB_PATH = ""
 
-SPOTIPY_CLIENT_ID = "ee2ff5ead33140f99f4fbf050f8c3620"
-SPOTIPY_CLIENT_SECRET = "e2b50f4490bb4b608827dbba888932dc"
-SPOTIPY_REDIRECT_URI = "https://example.org"
+SPOTIPY_CLIENT_ID = ""
+SPOTIPY_CLIENT_SECRET = ""
+SPOTIPY_REDIRECT_URI = ""
