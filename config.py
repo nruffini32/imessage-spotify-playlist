dd5ce366c685f2d40ccc57bc2d447fea1376fc05
nruffini32 <71286321+nruffini32@users.noreply.github.com>
2024-01-23 19:33:28 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
+# Name of iMessage group chat
 GROUP_CHAT = ""
+
+# Path to chat.db file - default location is "/Users/[profile_name]/Library/Messages/chat.db"
 DB_PATH = ""
 
+# Credentials from Twitter Developer App
 SPOTIPY_CLIENT_ID = ""
 SPOTIPY_CLIENT_SECRET = ""
 SPOTIPY_REDIRECT_URI = ""
